76dbd92522aa4c9b073e1acda083bb4fb7ff7b08
Quoc-Hwng <phamhung151020@gmail.com>
2022-09-16 17:57:27 +0700

    images picker on add product screen

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -12,7 +12,7 @@ void showSnackBar(BuildContext context, String text) {
   );
 }
 
-Future<List<File>> pickImage() async {
+Future<List<File>> pickImages() async {
   List<File> images = [];
   try {
     var files = await FilePicker.platform.pickFiles(
