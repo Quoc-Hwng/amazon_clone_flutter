df98942b198cdc423571893c4305a884d972ce2b
Quoc-Hwng <phamhung151020@gmail.com>
2022-09-17 21:44:13 +0700

    admin show all products screen, services, server

--- lib/features/admin/screens/posts_screen.dart
+++ lib/features/admin/screens/posts_screen.dart
@@ -1,5 +1,9 @@
+import 'package:amazon_clone_tutorial/common/widgets/loader.dart';
+import 'package:amazon_clone_tutorial/features/account/widgets/single_product.dart';
 import 'package:flutter/material.dart';
 
+import '../../../models/product.dart';
+import '../services/admin_service.dart';
 import 'add_product_screen.dart';
 
 class PostsScreen extends StatefulWidget {
@@ -10,22 +14,66 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
+  List<Product>? products;
+  final AdminServices adminServices = AdminServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllProducts();
+  }
+
+  fetchAllProducts() async {
+    products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
+
   void navigateToAddProduct() {
     Navigator.pushNamed(context, AddProductScreen.routeName);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text('Products'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: navigateToAddProduct,
-        tooltip: 'Add product',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: GridView.builder(
+              itemCount: products!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final productData = products![index];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(
+                        image: productData.images[0],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Text(productData.name,
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.delete_outline)),
+                      ],
+                    )
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: navigateToAddProduct,
+              tooltip: 'Add product',
+              child: const Icon(Icons.add),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
